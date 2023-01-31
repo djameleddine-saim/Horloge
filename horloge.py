c8e23417f6cd81c4fc6c31f940d27cb705b8a49b
djamel <djamel-eddine.saim@laplateforme.io>
2023-01-31 20:01:00 +0100

    l'horloge

--- horloge.py
+++ horloge.py
@@ -13,9 +13,9 @@ alarm_time = ""
 
 def time():
     if format.get() == "AM/PM":
-        string = strftime('%I:%M:%S PM')
+        string = strftime('%I:%M:%S %P')
     else:
-        string = strftime('%H:%M:%S AM')
+        string = strftime('%H:%M:%S')
     if alarm_set and string == alarm_time:
         alarm()
     label.config(text=string)
@@ -40,7 +40,7 @@ def set_alarm():
     global alarm_set, alarm_time
     alarm_time = alarm_entry.get()
     if format.get() == "AM/PM":
-        alarm_time = alarm_time + " " + am_pm.get()
+        alarm_time = alarm_time   # régler l'heure au niveaux des 24H
     alarm_set = True
 
 
@@ -52,7 +52,7 @@ def clear_alarm():
 
 def set_time():
     global current_time
-    current_time = set_time_entry.get() + " " + set_am_pm.get()
+    current_time = set_time_entry.get()
     label.config(text=current_time)
 
 set_time_frame = Frame(root)
@@ -61,8 +61,7 @@ set_time_frame.grid(row=3, columnspan=2)
 set_time_entry = Entry(set_time_frame)
 set_time_entry.pack(side=LEFT)
 
-set_am_pm = Combobox(set_time_frame, values=["AM", "PM"])
-set_am_pm.pack(side=LEFT)
+
 
 set_time_button = Button(set_time_frame, text="Modifier l'heure", command=set_time)
 set_time_button.pack(side=LEFT)
@@ -72,7 +71,7 @@ set_time_button.pack(side=LEFT)
 label = Label(root, font=("ds-digital", 60), background="black", foreground="cyan")
 label.grid(row=0, columnspan=4)
 
-toggle_format_button = Button(root, text="AM/PM", command=toggle_format)
+toggle_format_button = Button(root, text="12H/24H", command=toggle_format)
 toggle_format_button.grid(row=1, columnspan=4)
 
 alarm_frame = Frame(root)
@@ -81,8 +80,6 @@ alarm_frame.grid(row=2, columnspan=2)
 alarm_entry = Entry(alarm_frame)
 alarm_entry.pack(side=LEFT)
 
-am_pm = Combobox(alarm_frame, values=["AM", "PM"])
-am_pm.pack(side=LEFT)
 
 set_alarm_button = Button(alarm_frame, text="Régler L'alarme", command=set_alarm)
 set_alarm_button.pack(side=LEFT)
@@ -92,3 +89,4 @@ clear_alarm_button.pack(side=LEFT)
 
 time()
 mainloop()
+
